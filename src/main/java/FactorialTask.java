7e3e9d666f768d1697426c57346ce610b972564b
vyark <39518250+vyark@users.noreply.github.com>
2022-02-24 16:58:06 +0100

    Factorial refactor changes

---
+++ src/main/java/FactorialTask.java
@@ -0,0 +1,22 @@
+import com.google.common.math.BigIntegerMath;
+
+import java.math.BigInteger;
+import java.util.concurrent.RecursiveTask;
+
+public class FactorialTask extends RecursiveTask<BigInteger> {
+    private BigInteger number;
+
+    public FactorialTask(BigInteger number) {
+        this.number = number;
+    }
+
+    @Override
+    public BigInteger compute() {
+        if (number.compareTo(BigInteger.valueOf(1)) <= 0) {
+            return BigIntegerMath.factorial(number.intValue());
+        }
+        FactorialTask subtask = new FactorialTask(number.subtract(BigInteger.ONE));
+        subtask.fork();
+        return number.multiply(subtask.join());
+    }
+}
